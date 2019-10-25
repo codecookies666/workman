a511486c0c68e3ddb0ebb3868c51e1a39cb2cf39
codecookies666 <codecookies@163.com>
2019-10-25 16:34:05 +0800

    first commit

---
+++ server.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/10/25
+ * Time: 16:15
+ */
+define('APP_PATH', __DIR__ . '/application/');
+define('BIND_MODULE','push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
